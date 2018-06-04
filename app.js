8069de5308b6114a2738d20cb9bf7e6d4e8fb49f
lcr <1558531230@qq.com>
2018-06-04 15:17:16 +0800

    finished

---
+++ app.js
@@ -0,0 +1,13 @@
+/*
+ * Created by liuchaorun
+ * Date 18-6-4
+ * Time 下午3:13
+ **/
+const koa = require('koa');
+const koa_static = require('koa-static');
+
+app = new koa();
+
+app.use(koa_static(__dirname+'/html'));
+
+app.listen(3001);
